2f202063705262b35877e379ac3a161d2b63f6a4
kirkol <antonina.kirkalo-kowerczyk@infopark.de>
2018-09-18 14:28:56 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -30,6 +30,16 @@ app.post('/todos', (req, res) => {
   })
 })
 
+//routing dla adresu '/todos', ale dla metody GET
+//pobierze wszystkie dokumenty (wiersze) kolekcji (tabeli) todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
+
 app.listen(3000, () => {
   console.log('Started on port 3000')
 })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,6 +12,15 @@ beforeEach((done) => {
 })
 
 describe('POST /todos', () => {
+  // test sprawdzajacy czy jesli wyslemy request metoda POST do bazy
+  // ktory w polu text bedzie mial 'Test todo text'
+  // to serwer zwroci status = 200
+  // a w odpowiedzi z serwera (res.body.text) bedzie nasz wprowadzony text
+  // poza tym sprawdzane jest tez, czy w bazie pojawil sie nowy dokument (wiersz)
+  // UWAGA: sprawdzane jest to na bazie, ktora jest czysta, tj. nie ma zadnych dokumentow
+  // bo najpierw dziala funkcja beforeEach(), ktora czysci cala baze
+  // dzieki temu mozna sprawdzic warunek, czy kolekcja (tabela) ma teraz 1 dokument (wiersz)
+  // sprawdzane jest tez czy pierwszy dokument zawiera nasz text
   it('should create a new todo', (done) => {
     const text = 'Test todo text'
 
@@ -34,7 +43,10 @@ describe('POST /todos', () => {
       }).catch((e) => done(e))
     })
   })
-
+  // test sprawdzajacy czy jesli wyslemy do bazy pusty (nieprawidlowy) dokument (wiersz)
+  // to serwer powinien zwrocic nam status 400, do bazy nic nie powinno sie zapisac
+  // (bo pusty obiekt jest niezgodny z modelem todo), wiec kolekcja (tabela)
+  // powinna miec 0 elementow
   it('Should not create todo with invalid body data', (done) => {
     request(app)
     .post('/todos')
